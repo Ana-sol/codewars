75ed93eb53a41e6344181bf2a78351309bb741fc
Anastasiya Solovyova <rabtsavaas@gmail.com>
2024-03-13 00:21:45 +0300

    extractDomain

---
+++ extractDomain.js
@@ -0,0 +1,12 @@
+//task
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+//solution
+
+let domainName=(url)=>url.replace('www.','').replace('https://','').replace('http://','').split('.')[0]
+
